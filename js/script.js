c80237f53fa6910ecbe0bf915071c2caed3d60ab
John Torres <john.torres.hurtado@gmail.com>
2019-01-02 09:04:58 -0800

    added right order

--- js/script.js
+++ js/script.js
@@ -1,39 +1,39 @@
 
-$('container').append(
-  `<div card fw hsd>
-    <div class='block-one' vss>
-      <h2> 1. Input your commitment ammount </h2>
-
-      <p alt>A commitment ammount will help us calculate the estimated cost of using Leap. A larger commitment value the higher savings you will get.<br></p> <p> To know how we calculate our costs, <a>use our online calculator</a></p><br>
-
-      <div class='input-currency'>
-        <label> Estimated Commitment (Min. $5000.00)</label>
-        <input class='calc0' pattern="[0-9]*" type='text' placeholder='min. 5,000'/>
-      </div>
-    </div>
-
-    <img logo height=300px src='img/bg.png'/>
-   </div>
-  `
-)
-
-
-//input operations
-$('.calc0').on('input', function(){
-  price = parseFloat(this.value);
-
-  //month calc
-  data.map(value=>
-    $('.calc').text(`$ ${new Intl.NumberFormat().format(
-      Math.round(price * value.perc)) }.00`
-    )
-  );
-
-  //year calc
-  data.map( value=>
-    $('.calc2').text(`$ ${new Intl.NumberFormat().format(
-      Math.round(price * value.perc*12))}.00`
-    )
-  );
-
-})
+// $('container').append(
+//   `<div card fw hsd>
+//     <div class='block-one' vss>
+//       <h2> 1. Input your commitment ammount </h2>
+//
+//       <p alt>A commitment ammount will help us calculate the estimated cost of using Leap. A larger commitment value the higher savings you will get.<br></p> <p> To know how we calculate our costs, <a>use our online calculator</a></p><br>
+//
+//       <div class='input-currency'>
+//         <label> Estimated Commitment (Min. $5000.00)</label>
+//         <input class='calc0' pattern="[0-9]*" type='text' placeholder='min. 5,000'/>
+//       </div>
+//     </div>
+//
+//     <img logo height=300px src='img/bg.png'/>
+//    </div>
+//   `
+// )
+//
+//
+// //input operations
+// $('.calc0').on('input', function(){
+//   price = parseFloat(this.value);
+//
+//   //month calc
+//   data.map(value=>
+//     $('.calc').text(`$ ${new Intl.NumberFormat().format(
+//       Math.round(price * value.perc)) }.00`
+//     )
+//   );
+//
+//   //year calc
+//   data.map( value=>
+//     $('.calc2').text(`$ ${new Intl.NumberFormat().format(
+//       Math.round(price * value.perc*12))}.00`
+//     )
+//   );
+//
+// })
